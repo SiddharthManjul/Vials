609d13214a12ca4656459eb485af3839136f8716
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-07-04 23:32:31 +0530

    Envio Indexing Support

---
+++ app/indexing/config.ts
@@ -0,0 +1,19 @@
+// Check if an address was provided as a command-line argument
+if (process.argv.length < 3) {
+  console.error("Please provide Monad address as a parameter.");
+  process.exit(1);
+}
+
+// Get the target address from command-line arguments and validate it
+const targetContract = process.argv[2].toLowerCase();
+
+if (!/^0x[a-fA-F0-9]{40}$/.test(targetContract)) {
+  console.error("Invalid Monad address provided.");
+  process.exit(1);
+}
+
+export const holdDispalyThreshold = 10;
+export const hasIndexedToAndFromTopics =
+  process.env.NOT_INDEXED_TO_FROM != "true"; // If you want to run this on NFTs such as crypto-kitties, you'll need this on since those NFTs don't conform to the standard.
+export const hyperSyncEndpoint = "https://monad-testnet.rpc.hypersync.xyz/"; // More networks can be found here: https://docs.envio.dev/docs/HyperSync/hypersync-supported-networks
+export { targetContract };
